2d338c39a11995d6cc30cab05ceb13ae98eb2bc6
Pinardel Nina <pinardel2@mmi-dev.unilim.fr>
2025-04-04 13:57:45 +0200

    font

--- admin/data/datafilm.js
+++ admin/data/datafilm.js
@@ -3,7 +3,7 @@ let HOST_URL = "https://mmi.unilim.fr/~pinardel2/SAE2.03-Pinardel";
 let DataFilm = {}; 
 
 DataFilm.requestFilms = async function () { 
-  let answer = await fetch(HOST_URL + "/server/script.php?todo=addFilm"); 
+  let answer = await fetch(HOST_URL + "/server/script.php?todo=addFilm");
   let data = await answer.json();
   return data;
 };
--- app/component/film/script.js
+++ app/component/film/script.js
@@ -1,6 +1,10 @@
 let templateFile = await fetch("./component/film/template.html");
 let template = await templateFile.text();
 
+let templateLiFile = await fetch("./component/film/template_li.html");
+let templateLi = await templateLiFile.text();
+
+
 let Films = {};
 
 Films.format = function (films) {
@@ -12,6 +16,13 @@ Films.format = function (films) {
     html += filmHtml;
   });
   return html;
+};
+
+Films.formatLi = function (films) {
+  let filmContent = Films.format(films); // Génère le HTML des films
+  let liHtml = templateLi;
+  liHtml = liHtml.replace("{{film}}", filmContent); // Insère les films dans le conteneur
+  return liHtml;
 };
 
 export { Films };
